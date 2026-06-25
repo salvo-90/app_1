84207b5d98dcb807a5390aadc0079c556e491904
salvo-90 <62749241+salvo-90@users.noreply.github.com>
2026-06-25 16:05:55 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-from flask import Flask, send_from_directory
+from flask import Flask, send_from_directory, jsonify
+import pandas as pd
 
-app = Flask(__name__)   # 👈 QUESTA RIGA MANCAVA
+app = Flask(__name__)
 
 @app.route("/")
 def home():
     return send_from_directory(".", "index.html")
+
+@app.route("/api/excel")
+def excel():
+    df = pd.read_excel("dati.xlsx")
+    return jsonify(df.to_dict(orient="records"))
 
 if __name__ == "__main__":
     app.run()
