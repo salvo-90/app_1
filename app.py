bc0153fd8588abc76100fac7273b1f8046e17d6e
salvo-90 <62749241+salvo-90@users.noreply.github.com>
2026-06-25 16:03:10 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,10 @@
+from flask import Flask, send_from_directory
+
+app = Flask(__name__)   # 👈 QUESTA RIGA MANCAVA
+
 @app.route("/")
 def home():
     return send_from_directory(".", "index.html")
+
+if __name__ == "__main__":
+    app.run()
