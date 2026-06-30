c65aa39c41b13403e9a5325226a0d82f4b3d13c3
salvo-90 <62749241+salvo-90@users.noreply.github.com>
2026-06-30 14:53:54 +0200

    Update app.py

--- app.py
+++ app.py
@@ -8,17 +8,29 @@ def home():
     return send_from_directory(".", "index.html")
 
 
+
 @app.route("/api/excel")
 def get_excel():
 
     anno = request.args.get("anno")
+    nome = request.args.get("nome")
 
     df = pd.read_excel("dati.xlsx")
 
     if anno:
         df = df[df["Anno"] == int(anno)]
 
+    if nome:
+        df = df[
+            df["Nome"].str.contains(
+                nome,
+                case=False,
+                na=False
+            )
+        ]
+
     return df.to_dict(orient="records")
+
 
 if __name__ == "__main__":
     app.run()
